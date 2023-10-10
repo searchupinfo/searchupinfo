f421c724b3195c023e370df37ae81c69f4eb6ef4
Seth Deegan <jayandseth@gmail.com>
2023-10-10 03:40:40 +0000

    change homescreen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <section>
     <h1>
-        Welcome to SearchUpInfo!
+        Welcome to searchup.info!
     </h1>
     <p>This is an encyclopedia where all the articles are written by an PaLM2.</p>
     <p>Search for an article at the top left and wait for it to be generated! (search limited to 30 characters)</p>
