f9e1eef6ca57a81789267ea3241bbc1cf73db482
Seth Deegan <jayandseth@gmail.com>
2023-10-10 02:29:53 +0000

    fix typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
     <h1>
         Welcome to SearchUpInfo!
     </h1>
-    <p>This is an encyclopedia where the articles are written by an PaLM2.</p>
+    <p>This is an encyclopedia where all the articles are written by an PaLM2.</p>
     <p>Search for an article at the top left and wait for it to be generated!</p>
 </section>
