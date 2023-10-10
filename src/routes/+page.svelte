0e9c20d1a8e38ef90458c3e631822c5555d0a8fd
Seth Deegan <jayandseth@gmail.com>
2023-10-10 04:25:53 +0000

    fix homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
     <h1>
         Welcome to searchup.info!
     </h1>
-    <p>This is an encyclopedia where all the articles are written by an PaLM2.</p>
+    <p>This is an encyclopedia where all the articles are written by PaLM2.</p>
     <p>Search for an article at the top right and wait for it to be generated! (search limited to 30 characters)</p>
 </section>
