25c199bab0a0dcab40baa167afa4aa7726193517
Seth Deegan <jayandseth@gmail.com>
2023-10-14 15:48:54 +0000

    update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
     <h1 style="font-size: 4rem;">
         Welcome to searchup!
     </h1>
-    <p style="font-size: 1.5rem;text-align: center">This is an encyclopedia where all the articles are written by PaLM2. <br><br>Search for an article at the top right and wait for it to be generated!<br><br>Generation takes a minute or two and the search is limited to 30 characters.</p>
+    <p style="font-size: 1.5rem;text-align: center">This is an encyclopedia where all the articles are written by PaLM2. <br><br>Search for an article at the top right and wait for it to be generated!<br><br>Generation takes a minute or two and the search is limited to 30 characters.<br><br><a href="https://github.com/searchupinfo/searchupinfo">GitHub Repository</a></p>
 </section>
